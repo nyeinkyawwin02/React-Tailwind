c2dfe2b5df1d8c5dad56b656539819b3dee08000
juro-nkw02 <nyeinkyawwin11@gmail.com>
2023-06-28 00:27:49 +0630

    Initialize the repo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-blue-500 my-10 px-10'>Test</h1>
+    </>
+  )
+}
+
+export default App
